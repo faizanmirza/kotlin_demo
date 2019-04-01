4d05c4ce24dc2d70f3887b6c3df9901a76a5b0cc
Faizan Baig <faizan.baig@tenpearls.com>
2019-04-01 11:45:42 +0500

    - intial commit

---
+++ app/src/main/java/pk/kotlin/sample/activities/OnBoardingActivity.kt
@@ -0,0 +1,13 @@
+package pk.kotlin.sample.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import pk.kotlin.sample.R
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
---
+++ app/src/main/java/pk/kotlin/sample/fragments/OnBoardingStepOne.kt
@@ -0,0 +1,31 @@
+package pk.kotlin.sample.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pk.kotlin.sample.R
+
+class OnBoardingStepOne : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_on_boarding_step_one, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
